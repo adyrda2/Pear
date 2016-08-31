cbfa61f23e3d36df6bc22c27ab9407ada577b3b8
adyrda2 <adyrda2@gmail.com>
2016-08-30 20:57:37 -0500

    route to show page if logged in

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'omniauth-twitter'
 gem 'dotenv-rails'
+gem 'pry'
 
 group :production do
   gem 'pg'
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,6 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
     #persist user login through a session - store user id.
     session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in!"
+    redirect_to user, notice: "Signed in!"
   end
 end
