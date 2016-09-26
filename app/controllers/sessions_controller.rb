93f52de051f4437226505fb0afbb7ccbb0d1f957
adyrda2 <adyrda2@gmail.com>
2016-09-25 21:53:16 -0500

    Remove comment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    #persist user login through a session - store user id.
     session[:user_id] = user.id
     redirect_to user, notice: "Signed in!"
   end
