3a323cab62ba6bf908cff40d4f9ce4ab2762b2d5
Angela Dyrda <adyrda2@gmail.com>
2016-10-07 16:44:32 -0500

    Establish activerecord associations

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,3 @@
 class Room < ActiveRecord::Base
+  has_many :users, through: :users_rooms
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160829025905) do
+ActiveRecord::Schema.define(version: 20161007213732) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "summary"
+    t.string   "language"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
